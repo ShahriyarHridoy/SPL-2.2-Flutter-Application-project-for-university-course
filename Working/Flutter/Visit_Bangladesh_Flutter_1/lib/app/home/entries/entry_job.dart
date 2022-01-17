da1f2d09750f4c8f7c83505be4289fad80399616
ShahriyarHridoy <35798678+ShahriyarHridoy@users.noreply.github.com>
2022-01-18 03:16:52 +0600

    Visit_Bangladesh_Flutter_10_0000

--- Working/Flutter/Visit_Bangladesh_Flutter_1/lib/app/home/entries/entry_job.dart
+++
@@ -1,9 +0,0 @@
-import 'package:time_tracker_flutter_course/app/home/models/entry.dart';
-import 'package:time_tracker_flutter_course/app/home/models/job.dart';
-
-class EntryJob {
-  EntryJob(this.entry, this.job);
-
-  final Entry entry;
-  final Job job;
-}
